df8f3aca5e6ecb13c03d246aa4f731a617d9c9dd
d.ryabokon <d.ryabokon@gmail.com>
2026-02-13 17:46:38 +0200

    HITL

--- models/export_me.sh
+++ models/export_me.sh
@@ -1,1 +1,2 @@
-yolo export model=./yolov8n.pt format=onnx device=cpu imgsz=640
+#yolo export model=./yolov8n.pt format=onnx device=cpu imgsz=640
+yolo export model=./yolov8n.pt format=ncnn device=cpu imgsz=640
